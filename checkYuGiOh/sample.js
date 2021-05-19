a6f4ecabff635d00e2d603839773127dbf27fc64
ifeoluwa04 <81023465+ifeoluwa04@users.noreply.github.com>
2021-05-19 18:28:16 +0100

    Add files via upload

---
+++ checkYuGiOh/sample.js
@@ -0,0 +1,29 @@
+function checkYuGiOh(n) {
+    let arr = [];
+    if (!Number(n)) {
+      return `Invalid parameter: ${n}`;
+    }
+    for (let i = 1; i <= n; i++) {
+      if (i % 30 === 0) {
+        arr.push("yu-gi-oh");
+      } else if (i % 10 === 0) {
+        arr.push("yu-oh");
+      } else if (i % 6 === 0) {
+        arr.push("yu-oh");
+      } else if (i % 15 === 0) {
+        arr.push("yu-oh");
+      } else if (i % 2 === 0) {
+        arr.push("yu");
+      } else if (i % 3 === 0) {
+        arr.push("gi");
+      } else if (i % 5 === 0) {
+        arr.push("oh");
+      } else {
+        arr.push(i);
+      }
+    }
+  
+    return arr;
+  }
+  
+  
